a855113cde7bb94d03a294767564f42e0c36843f
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-07-21 11:54:27 +0200

    Display colors in SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 
 class SingleColorPalette extends Component {
+  _shades = this.gatherShades(this.props.palette, this.props.colorId);
+
+  gatherShades(palette, colorToFilterBy) {
+    let shades = [];
+    const allColors = palette.colors;
+    Object.keys(allColors).forEach(key => {
+      shades = shades.concat(
+        allColors[key].filter(color => color.id === colorToFilterBy)
+      );
+    });
+    return shades.slice(1);
+  }
+
   render() {
+    const colorBoxes = this._shades.map(color => (
+      <ColorBox
+        key={color.name}
+        name={color.name}
+        background={color.hex}
+        showLink={false}
+      />
+    ));
+    console.log(colorBoxes);
     return (
-      <div>
+      <div className="Palette">
         <h1>Single Color Palette</h1>
+        <div className="Palette-colors">{colorBoxes}</div>
       </div>
     );
   }

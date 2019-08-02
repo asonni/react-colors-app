e7bbe318e9815ea97f46eb80c3cc70893d18a432
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-08-02 21:48:53 +0200

    Add responsive helper and style ColorBox

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,6 +1,8 @@
 import chroma from 'chroma-js';
 import { makeStyles } from '@material-ui/core/styles';
 
+import sizes from './sizes';
+
 export default makeStyles({
   ColorBox: {
     width: '20%',
@@ -13,6 +15,18 @@ export default makeStyles({
     '&:hover button': {
       opacity: '1',
       transition: '0.5s'
+    },
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: props => (props.showingFullPalette ? '20%' : '50%')
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: props => (props.showingFullPalette ? '10%' : '50%')
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: props => (props.showingFullPalette ? '5%' : '10%')
     }
   },
   copyText: {

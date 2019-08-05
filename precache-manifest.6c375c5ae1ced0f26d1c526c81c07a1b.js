7e6156f9371a00d7e02e6bf669f3f14a0ea77c74
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-08-05 13:53:24 +0200

    Updates

---
+++ precache-manifest.6c375c5ae1ced0f26d1c526c81c07a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991da1f5da2c35a59c3c0bdb77c2c513",
+    "url": "/react-colors-app/index.html"
+  },
+  {
+    "revision": "ddf969240e4e81e9617d",
+    "url": "/react-colors-app/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "767f0aad8f5cc32f2cd5",
+    "url": "/react-colors-app/static/css/main.157037d3.chunk.css"
+  },
+  {
+    "revision": "ddf969240e4e81e9617d",
+    "url": "/react-colors-app/static/js/2.4feda87d.chunk.js"
+  },
+  {
+    "revision": "767f0aad8f5cc32f2cd5",
+    "url": "/react-colors-app/static/js/main.c1198ce1.chunk.js"
+  },
+  {
+    "revision": "33ae901c7e870ee49837",
+    "url": "/react-colors-app/static/js/runtime~main.c591a86d.js"
+  },
+  {
+    "revision": "898d17314351b4d722c32e4ed5d3b6a3",
+    "url": "/react-colors-app/static/media/bg.898d1731.svg"
+  }
+]);
